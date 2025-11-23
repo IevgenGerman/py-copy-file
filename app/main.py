89b36fd602ebc22ace550b2ea751cabf0d341f3c
Ievgen German <german.kbrza@gmail.com>
2025-11-23 20:40:10 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def copy_file(command: str) -> None:
+    list_com = command.split()
+    if len(list_com) < 3:
+        return
+    if list_com[0] != "cp":
+        return
+    if list_com[1] == list_com[2]:
+        return
+    try:
+        with (open(list_com[1],
+                   "r",
+                  newline="",
+                  encoding="utf-8") as f_r,
+              open(list_com[2],
+                   "w",
+                   newline="",
+                   encoding="utf-8") as f_w):
+            for line in f_r:
+                f_w.write(line)
+    except FileNotFoundError:
+        return
